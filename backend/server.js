8e2a72dad81a3bbb17584b89f0e1abb0cd229cc1
Eric L <ericliao57@gmail.com>
2025-02-16 00:46:33 +1100

    ➖

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const { exec } = require('child_process');
 const app = express();
 const port = 5123;
 
+const resolveIP = "127.0.0.1"
 const resolverPort = 5321;
 
 app.use(cors({
@@ -19,16 +20,31 @@ app.post('/api/query', (req, res) => {
         return res.status(400).json({ error: "domainName missing" });
     }
 
-    const digCommand = `dig ${domainName} @127.0.0.1 -p ${resolverPort}`;
+    // const digCommand = `dig ${domainName} @${resolverIP} -p ${resolverPort}`;
 
-    exec(digCommand, (error, stdout, stderr) => {
+    // exec(digCommand, (error, stdout, stderr) => {
+    //     if (error) {
+    //         console.error(`Error executing dig: ${error.message}`);
+    //         return res.status(500).json({ error: "Failed to resolve domain" });
+    //     }
+
+    //     if (stderr) {
+    //         console.warn(`dig stderr: ${stderr}`);
+    //     }
+        
+    //     res.json({ result: stdout })
+    // });
+
+    const command = `python3 client.py ${resolveIP} ${resolverPort} ${domainName}`;
+
+    exec(command, (error, stdout, stderr) => {
         if (error) {
-            console.error(`Error executing dig: ${error.message}`);
+            console.error(`Error executing client.py: ${error.message}`);
             return res.status(500).json({ error: "Failed to resolve domain" });
         }
 
         if (stderr) {
-            console.warn(`dig stderr: ${stderr}`);
+            console.warn(`client.py stderr: ${stderr}`);
         }
         
         res.json({ result: stdout })
