10d7c1e963372f1671b9f530f4e52409ac9c4bcd
Eric L <email@email.com>
2025-03-08 02:06:34 +1100

    teme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import './App.css'
 import { About } from './About'
 
 function App() {
-  const expressServerPort = 5123
+  const expressServerPort = 5123;
+  const [theme, setTheme] = useState("autumn");
   const [loading, setLoading] = useState(false);
   const [input, setInput] = useState("");
   const [inputDesc, setInputDesc] = useState({
@@ -78,6 +79,35 @@ function App() {
   return (
     <>
       <About />
+      <label className="flex cursor-pointer gap-2 absolute top-0 left-0 m-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round">
+          <circle cx="12" cy="12" r="5" />
+          <path
+            d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+        </svg>
+        <input type="checkbox" value="sunset" className="toggle theme-controller" onChange={e => e.target.checked ? setTheme("sunset") : setTheme("autumn")} />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round">
+          <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+        </svg>
+      </label>
 
       <div role="alert" className={`alert alert-error absolute bottom-0 left-0 m-4 ${showError}`}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
@@ -86,7 +116,7 @@ function App() {
         <span>Error with dns resolution</span>
       </div>
 
-      <main data-theme="autumn" className='bg-transparent mx-auto w-full max-w-4xl'>
+      <main data-theme={theme} className='bg-transparent mx-auto w-full max-w-4xl my-10'>
         <div className='flex flex-col sm:flex-row gap-4 m-2 sm:items-end'>
           <fieldset className="flex grow mb-1">
             <legend className="fieldset-legend text-primary">{inputDesc.label}</legend>
@@ -103,7 +133,8 @@ function App() {
             <fieldset className="fieldset grow basis-0">
               <legend className="fieldset-legend text-primary">Record Type</legend>
               <select defaultValue="A" className="select border-secondary" onChange={e => setRecordType(e.target.value)}>
-                <option value="A">A (a host address)</option>
+                <option value="A">A (host address)</option>
+                <option value="NS">NS (authoritative name server)</option>
                 <option value="MX">MX (mail exchange)</option>
                 <option value="CNAME">CNAME (canonical name for alias)</option>
                 <option value="PTR">PTR (reverse lookup with IPv4)</option>
@@ -125,11 +156,11 @@ function App() {
         </div>
         <div className="mockup-browser border-secondary border bg-secondary mx-2 min-h-40">
           <div className="mockup-browser-toolbar">
-            <div className="input text-accent-content bg-base-100 border-secondary grow">{JSON.stringify(question)}</div>
+            <div className="input text-base-content bg-base-100 border-secondary grow">{JSON.stringify(question)}</div>
           </div>
           <div className="grid place-content-center border-t border-base-300 min-h-80 bg-base-100 ">
             {loading && <span className="loading loading-spinner loading-xl"></span>}
-            <p className='mx-4 text-wrap'>{answer}</p>
+            <pre className='my-4'><code className='mx-2'>{answer}</code></pre>
           </div>
         </div>
       </main>
