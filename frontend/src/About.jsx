a8ec35a42ab01d8f1fe956ec88d72f478f481152
Eric L <email@email.com>
2025-03-07 21:04:04 +1100

    f

---
+++ frontend/src/About.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+export function About() {
+  return (
+    <>
+      <button className="absolute right-0 top-0 btn text-base-content bg-transparent m-2" onClick={() => document.getElementById('my_modal_2').showModal()}>
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 64 64">
+          <path d="M 32 10 C 19.85 10 10 19.85 10 32 C 10 44.15 19.85 54 32 54 C 44.15 54 54 44.15 54 32 C 54 19.85 44.15 10 32 10 z M 32 14 C 41.941 14 50 22.059 50 32 C 50 41.941 41.941 50 32 50 C 22.059 50 14 41.941 14 32 C 14 22.059 22.059 14 32 14 z M 32 21 C 30.343 21 29 22.343 29 24 C 29 25.657 30.343 27 32 27 C 33.657 27 35 25.657 35 24 C 35 22.343 33.657 21 32 21 z M 32 30 C 30.895 30 30 30.896 30 32 L 30 42 C 30 43.104 30.895 44 32 44 C 33.105 44 34 43.104 34 42 L 34 32 C 34 30.896 33.105 30 32 30 z"></path>
+        </svg>
+        About
+      </button>
+      <dialog id="my_modal_2" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-xl">A DNS Desolver built in python</h3>
+          <p>Implemented according to the <a className="link" href="https://datatracker.ietf.org/doc/html/rfc1035">RFC1035</a></p>
+          <p className="py-4 text-xl underline">Client</p>
+          <p>You can use my custom query builder or <a className="link" href="https://linux.die.net/man/1/dig">dig</a></p>
+          <p className='py-4 text-lg underline'>Queries</p>
+          <p>Available record options include</p>
+          <ul className='ml-4'>
+            <li> - A</li>
+            <li> - MX</li>
+            <li> - CNAME</li>
+            <li> - PTR (only supports IPv4 for reverse lookups)</li>
+          </ul>
+          <p className="py-4 text-xl underline">Resolver</p>
+          <p>You can use my custom resolver or any of the listed resolvers like google's at 8.8.8.8</p>
+
+          <p className='mt-4 text-end text-xs'>All the code for this project is available on my <a className="link" href="https://github.com/RiceL123/DNS-resolver">GitHub</a></p>
+          <p className='mt-4 text-end text-xs'>Made by RiceL123</p>
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+          </form>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
+  )
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 
+import { About } from './About'
+
 function App() {
   const expressServerPort = 5123
   const [loading, setLoading] = useState(false)
@@ -31,43 +33,59 @@ function App() {
   }
 
   return (
-    <main data-theme="autumn" className='bg-transparent mx-auto w-full max-w-4xl'>
-      <div className='flex flex-col sm:flex-row gap-4 m-2 sm:items-end'>
-        <fieldset className="flex grow mb-1">
-          <legend className="fieldset-legend text-primary">Domain</legend>
-          <input type="text" className="input grow border-secondary" placeholder="Enter domain: example.com" value={domain} onChange={(e) => setDomain(e.target.value)} />
-        </fieldset>
-        <div className='flex gap-4 justify-around'>
-          <fieldset className="fieldset grow basis-0">
-            <legend className="fieldset-legend text-primary">Query Builders</legend>
-            <select defaultValue="Default" className="select border-secondary">
-              <option>Default</option>
-              <option>Dig</option>
-            </select>
+    <>
+      <About />
+
+      <main data-theme="autumn" className='bg-transparent mx-auto w-full max-w-4xl'>
+        <div className='flex flex-col sm:flex-row gap-4 m-2 sm:items-end'>
+          <fieldset className="flex grow mb-1">
+            <legend className="fieldset-legend text-primary">Domain</legend>
+            <input type="text" className="input grow border-secondary" placeholder="Enter domain: example.com" value={domain} onChange={(e) => setDomain(e.target.value)} />
           </fieldset>
-          <fieldset className="fieldset grow basis-0">
-            <legend className="fieldset-legend text-primary">Resolvers</legend>
-            <select defaultValue="Default" className="select border-secondary">
-              <option>Default</option>
-              <option>Cloudflare (1.1.1.1)</option>
-              <option>Google (8.8.8.8)</option>
-              <option>Quad9 (9.9.9.9)</option>
-              <option>OpenDNS (208.67.222.222)</option>
-            </select>
-          </fieldset>
+          <div className='flex gap-4 justify-around'>
+            <fieldset className="fieldset grow basis-0">
+              <legend className="fieldset-legend text-primary">Query Builders</legend>
+              <select defaultValue="RiceL123's querier" className="select border-secondary">
+                <option>RiceL123's querier</option>
+                <option>Dig</option>
+              </select>
+            </fieldset>
+            <fieldset className="fieldset grow basis-0">
+              <legend className="fieldset-legend text-primary">Record Type</legend>
+              <select defaultValue="A" className="select border-secondary">
+                <option>A</option>
+                <option>MX</option>
+                <option>CNAME</option>
+                <option>PTR (reverse lookup)</option>
+              </select>
+            </fieldset>
+            <fieldset className="fieldset grow basis-0">
+              <legend className="fieldset-legend text-primary">Resolvers</legend>
+              <select defaultValue="RiceL123's resolver" className="select border-secondary">
+                <option>RiceL123's resolver</option>
+                <option>Cloudflare (1.1.1.1)</option>
+                <option>Google (8.8.8.8)</option>
+                <option>Quad9 (9.9.9.9)</option>
+                <option>OpenDNS (208.67.222.222)</option>
+              </select>
+            </fieldset>
+          </div>
         </div>
-        <button className="mb-1 btn text-primary-content bg-primary" onClick={handleQuery}>Submit</button>
-      </div>
-      <div className="mockup-browser border-secondary border bg-secondary mx-2">
-        <div className="mockup-browser-toolbar">
-          <div className="input text-accent-content bg-base-100 border-secondary">{domain}</div>
+        <div className='max-w-50 mx-auto flex justify-center'>
+          <button className="mb-1 btn text-primary-content bg-primary grow" onClick={handleQuery}>Lookup</button>
         </div>
-        <div className="grid place-content-center border-t border-base-300 h-80 bg-base-100">
-          <p>{loading ? "loading" : "not loading"}</p>
-          <p>{answer}</p>
+        <div className="mockup-browser border-secondary border bg-secondary mx-2">
+          <div className="mockup-browser-toolbar">
+            <div className="input text-accent-content bg-base-100 border-secondary">{domain}</div>
+          </div>
+          <div className="grid place-content-center border-t border-base-300 h-80 bg-base-100">
+            {loading && <span className="loading loading-spinner loading-xs"></span>}
+            <p>{loading ? "loading" : "not loading"}</p>
+            <p>{answer}</p>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
 
